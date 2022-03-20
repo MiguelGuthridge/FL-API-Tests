cbce93bf3738bbe25dfb6a7ee84458503dc04af1
Miguel Guthridge <hdsq@outlook.com.au>
2022-03-20 20:45:15 +1100

    Add out of bounds index checks for playlist

--- tests/playlist/index.py
+++ tests/playlist/index.py
@@ -4,7 +4,7 @@ tests > playlist > index
 Tests for indexing of playlist
 """
 
-from fltest import flSimpleTest
+from fltest import flSimpleTest, Raises
 
 import playlist
 
@@ -17,3 +17,15 @@ def minIndex():
 def maxIndex():
     """Access index 500"""
     playlist.getTrackName(500)
+
+@flSimpleTest()
+def lowerBounds():
+    """Test accessing out of bounds index 0"""
+    with Raises(TypeError):
+        playlist.getTrackName(0)
+
+@flSimpleTest()
+def upperBounds():
+    """Test accessing out of bounds index 501"""
+    with Raises(TypeError):
+        playlist.getTrackName(501)
